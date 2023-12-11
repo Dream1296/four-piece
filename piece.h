d4618b684001e1060c9d9798880bb46d71eebad9
Dream1296 <84652450+Dream1296@users.noreply.github.com>
2023-12-11 15:52:39 +0800

    Add files via upload

---
+++ piece.h
@@ -0,0 +1,26 @@
+#ifndef MYHEADER_H 
+#define MYHEADER_H 
+
+
+
+
+
+void picount(int player);       //刷新各方棋子
+int win();
+void prpiece();
+void gps();
+int AMobile();
+int shuru(int player);
+int players(char q, int p);
+int gpspl(int x, int y, int player);
+void logic(int player);
+
+
+
+
+
+
+
+
+#endif
+
